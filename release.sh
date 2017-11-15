cea007b27e3cf8b4a357bb855ef34d6160e66824
honnix <hxliang1982@gmail.com>
2017-11-15 01:00:14 +0100

    release script

---
+++ release.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -x
+
+mvn -s ~/.m2/settings.xml.honnix release:prepare -DpushChanges=false
+
+mvn -s ~/.m2/settings.xml.honnix release:perform -DlocalCheckout=true \
+  -Darguments="-DskipTests -Ddockerfile.skip=true"
+
+git push origin HEAD
+git push --tags
